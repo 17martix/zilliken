a51a67b7d70b61357282bf01dd85361acb53eb85
lavinie257 <eulavinie@gmail.com>
2021-02-04 16:25:09 +0200

    faire le menu

--- lib/Pages/MenuPage.dart
+++ lib/Pages/MenuPage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:zilliken/Components/ZAppBar.dart';
+import 'package:zilliken/Helpers/Styling.dart';
 
 class MenuPage extends StatefulWidget {
   @override
@@ -8,6 +10,89 @@ class MenuPage extends StatefulWidget {
 class _MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
-    return Text("menu");
+    return Scaffold(
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "All/Tout",
+                      ),
+                      Container(
+                        height: 2,
+                        width: 20,
+                        color: Color(Styling.accentColor),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Petit dejeuner",
+                        style: TextStyle(
+                          color: Color(Styling.textColor).withOpacity(0.5),
+                        ),
+                      ),
+                      Container(
+                        height: 2,
+                        width: 20,
+                        color: Color(Styling.accentColor),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  "Dinner",
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8.0),
+                height: 1,
+                color: Color(Styling.accentColor),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("Petit dejeuner"),
+              ),
+              Container(
+                padding: const EdgeInsets.all(15.0),
+                height: 1,
+                color: Color(Styling.accentColor),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(8),
+              children: <Widget>[
+                ListTile(
+                  title: Text("The vert"),
+                  subtitle: Text("500"),
+                  trailing: FlatButton(
+                    child: Text("+"),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
