a85818534a32160e142940179df0f21eb254d325
17martix <abeho14@gmail.com>
2021-02-05 11:06:54 +0200

    added field model

---
+++ lib/Models/MenuItem.dart
@@ -0,0 +1,21 @@
+class MenuItem {
+  String id;
+  String name;
+  String category;
+  int price;
+  int rank;
+  int global;
+  int availability;
+  int createdAt;
+
+  MenuItem({
+    this.category,
+    this.id,
+    this.name,
+    this.price,
+    this.rank,
+    this.global,
+    this.availability,
+    this.createdAt,
+  });
+}
