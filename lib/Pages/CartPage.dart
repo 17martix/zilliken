1bb1b812e017f7d52c4e4d22f21a8207315c7b9e
njsolana <solanajessy@gmail.com>
2021-02-05 11:07:48 +0200

    firebase_cartPage

--- lib/Pages/CartPage.dart
+++ lib/Pages/CartPage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:zilliken/Components/ZAppBar.dart';
 import 'package:zilliken/Helpers/Styling.dart';
@@ -212,6 +213,32 @@ class _CartPageState extends State<CartPage> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget orderlist() {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: users.snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return new ListView(
+          children: snapshot.data.documents.map((DocumentSnapshot document) {
+            return new ListTile(
+              title: new Text(document.data()['full_name']),
+              subtitle: new Text(document.data()['company']),
+            );
+          }).toList(),
+        );
+      },
     );
   }
 }
