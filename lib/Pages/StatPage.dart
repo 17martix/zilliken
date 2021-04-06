009a1d201b4313f365d80bee8193358f6c290448
njsolana <solanajessy@gmail.com>
2021-04-06 14:09:11 +0200

    creation des Pages

---
+++ lib/Pages/StatPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StatPage extends StatefulWidget {
+  @override
+  _StatPageState createState() => _StatPageState();
+}
+
+class _StatPageState extends State<StatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
